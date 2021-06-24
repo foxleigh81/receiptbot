f6d2bed840f7d1b3faa3f76b4e98b9dd193b633b
Alex Foxleigh <alex@alexfoxleigh.com>
2021-06-24 03:47:07 +0100

    v1.0.0 Saves files to downloads folder with caption as file name

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const TelegramBot = require('node-telegram-bot-api');
+const fs = require('fs');
 
 const botToken = process.env.TELEGRAM_BOT_TOKEN
 
@@ -16,9 +17,77 @@ bot.onText(/\/add (.+)/, (msg, match) => {
   
     const chatId = msg.chat.id;
     const resp = match[1]; // the captured "whatever"
-  
+    console.log(msg)
     // send back the matched "whatever" to the chat
     bot.sendMessage(chatId, `Added ${resp} file`);
 });
 
 
+const renameFile = (oldName, newName) => {
+    const matcher = /downloads\/(file_\d).\w+/
+    const match = oldName.match(matcher)[1]
+    return newName 
+        ? oldName.replace(match, newName)
+        : oldName
+}
+
+// bot.on('message', (msg) => {
+//     const chatId = msg.chat.id;
+
+//     if (msg.photo.length > 0) {
+//         // Photo received
+//         const lastPhoto = msg.photo.slice(-1)[0]
+//         bot.downloadFile(lastPhoto.file_id, './downloads')
+//         .then((path) => {
+//             const newName = msg.caption || null
+//             fs.rename(
+//                 path,
+//                 renameFile(path, newName),
+//                 () => {
+//                     console.log('file renamed')
+//                 }
+//             )
+//         })
+//     }
+
+//     if (msg.document && msg.document.file_name !== '') {
+//         // Other file received
+//         console.log(msg.document)
+//     }
+   
+//     // send a message to the chat acknowledging receipt of their message
+//     bot.sendMessage(chatId, 'Received your message');
+//   });
+
+bot.on('photo', (msg) => {
+    const chatId = msg.chat.id;
+    const lastPhoto = msg.photo.slice(-1)[0]
+    bot.downloadFile(lastPhoto.file_id, './downloads')
+    .then((path) => {
+        const newName = msg.caption || null
+        const renamedFile = renameFile(path, newName)
+        fs.rename(
+            path,
+            renamedFile,
+            () => {
+                bot.sendMessage(chatId, `File, ${renamedFile} saved`);
+            }
+        )
+    })
+});
+
+bot.on('document', (msg) => {
+    const chatId = msg.chat.id;
+    bot.downloadFile(msg.document.file_id, './downloads')
+    .then((path) => {
+        const newName = msg.caption || msg.document.file_name
+        const renamedFile = renameFile(path, newName)
+        fs.rename(
+            path,
+            renamedFile,
+            () => {
+                bot.sendMessage(chatId, `File, ${renamedFile} saved`);
+            }
+        )
+    })
+});
